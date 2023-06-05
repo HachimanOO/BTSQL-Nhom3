e0d1e2dae8869c984e6054023f2828df3ae6c96c
huytran2510 <135006250+huytran2510@users.noreply.github.com>
2023-06-05 22:10:38 +0700

    Update CustomerDAOImpl.java

--- Assignment3_Nhom3/src/main/java/fa/training/dao/CustomerDAOImpl.java
+++ Assignment3_Nhom3/src/main/java/fa/training/dao/CustomerDAOImpl.java
@@ -1,68 +1,95 @@
 package fa.training.dao;
 
 import fa.training.entities.Customer;
-import org.example.Database;
+import fa.training.main.Database;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerDAOImpl implements CustomerDAO{
     private Database database;
     public CustomerDAOImpl(){
-        this.database = new Database();
+        this.database =new Database();
     }
-    @Override
-    public List<Customer> getAllCustomers() {
-        List<Customer> listCustomer = new ArrayList<>();
-        String sql = "SELECT * FROM customer";
-        try {
-            Statement statement = database.getConnection().createStatement();
-            ResultSet rs = statement.executeQuery(sql);
-            while (rs.next()) {
-                Customer cus = new Customer(rs.getInt("customer_id"),rs.getString("customer_name"));
-                 listCustomer.add(cus);
-            }
-            for(Customer  cus : listCustomer ) {
-                System.out.println(cus);
-            }
-        } catch (SQLException e) {
-            throw  new RuntimeException(e);
-        }
-        return listCustomer;
-    }
+
+
+    static final List<Customer> customerList = new ArrayList<>();
+
 
     @Override
     public boolean addCustomer(Customer customer) {
-        String sql = "INSERT INTO customer (customer_id, customer_name) VALUES (?, ?)";
+        String sql = "INSERT INTO `customer`(`customerId`, `customerName`) VALUES (?,?)";
+        Database database = new Database();
         try {
-            PreparedStatement statement = database.getConnection().prepareStatement(sql);
-            statement.setInt(1, customer.getCustomerId());
-            statement.setString(2, customer.getCustomerName());
-            System.out.println("Added success");
-            return statement.execute();
+            Connection con = database.getConnection();
+            con.setAutoCommit(false);
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setInt(1,customer.getCustomerId());
+            preparedStatement.setString(2,customer.getCustomerName());
+            int rowsAffected = preparedStatement.executeUpdate();
+            con.commit();
+            con.setAutoCommit(true);
+            return rowsAffected > 0;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean deleteCustomer(int customerId) {
+        String sql ="DELETE FROM `customer` WHERE customerId = ?";
+        Database database = new Database();
+        try {
+            Connection con = database.getConnection();
+            con.setAutoCommit(false);
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setInt(1,customerId);
+            int rowsAffected = preparedStatement.executeUpdate();
+            con.commit();
+            con.setAutoCommit(true);
+            return rowsAffected > 0;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
 
+    public List<Customer> getAllCustomers(){
+        String sql = "SELECT * FROM customer";
+        try {
+            Statement statement = database.getConnection().createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            while (rs.next()) {
+                Customer customer = new Customer(rs.getInt("customerId"), rs.getString("customerName"));
+                customerList.add(customer);
+            }
+            for (Customer customer : customerList) {
+                System.out.println(customer);
+            }
 
-    @Override
-    public boolean deleteCustomer(int customerId) {
-        return false;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return customerList;
     }
-
     @Override
     public boolean updateCustomer(Customer customer) {
-        return false;
+        String sql = "UPDATE customer SET customerName = ? WHERE customerId = ?";
+        try {
+            Connection con = database.getConnection();
+            con.setAutoCommit(false);
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, customer.getCustomerName());
+            statement.setInt(2, customer.getCustomerId());
+            int rowsAffected = statement.executeUpdate();
+            con.commit();
+            con.setAutoCommit(true);
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static void main(String[] args) {
-        CustomerDAO test = new CustomerDAOImpl();
-        test.getAllCustomers();
-    }
+
 }
