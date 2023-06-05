506bef647dd7bd08f1f5f4d0f72797eecdfaaee9
huytran2510 <135006250+huytran2510@users.noreply.github.com>
2023-06-05 22:12:35 +0700

    Update OrderDAOImpl.java

--- Assignment3_Nhom3/src/main/java/fa/training/dao/OrderDAOImpl.java
+++ Assignment3_Nhom3/src/main/java/fa/training/dao/OrderDAOImpl.java
@@ -1,43 +1,65 @@
 package fa.training.dao;
 
-import fa.training.entities.Order;
-import org.example.Database;
-
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class OrderDAOImpl implements OrderDAO{
-    private Database database;
-    public OrderDAOImpl(){
-        this.database = new Database();
-    }
     @Override
     public List<Order> getAllOrdersByCustomerId(int customerId) {
-        return null;
+        String sql = "SELECT * FROM order WHERE customerId = ?";
+        Order order = null;
+        List<Order> orderList = new ArrayList<>();
+        try {
+            Database database = new Database();
+            PreparedStatement preparedStatement = database.getConnection().prepareStatement(sql);
+            preparedStatement.setInt(1, customerId);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                order = new Order(rs.getInt("orderId"), rs.getDate("date"), rs.getInt("customerId"),
+                        rs.getInt("employeeId"), rs.getDouble("total"));
+                orderList.add(order);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return orderList;
     }
-
     @Override
     public boolean addOrder(Order order) {
-        try {
-            String sql = "INSERT INTO Orders (order_id, order_date, customer_id, employee_id, total) VALUES (?, ?, ?, ?, ?)";
-            PreparedStatement statement = database.getConnection().prepareStatement(sql);
-            statement.setInt(1, order.getOrderId());
-            statement.setDate(2, (Date) order.getOrderDate());
-            statement.setInt(3, order.getCustomerId());
-            statement.setInt(4, order.getEmployeeId());
-            statement.setDouble(5, order.getTotal());
-
-            int rowsAffected = statement.executeUpdate();
-            return rowsAffected > 0;
-        } catch (SQLException e) {
+        String sql = "INSERT INTO `order`(`orderId`, `date`, `customerId`, `employeeId`, `total`) VALUES (?,?,?,?,?)";
+        Database database = new Database();
+        try{
+            PreparedStatement preparedStatement = database.getConnection().prepareStatement(sql);
+            preparedStatement.setInt(1,order.getOrderId());
+            preparedStatement.setDate(2, (Date) order.getOrderDate());
+            preparedStatement.setInt(3,order.getCustomerId());
+            preparedStatement.setInt(4,order.getEmployeeId());
+            preparedStatement.setDouble(5,order.getTotal());
+            return preparedStatement.execute();
+        }catch (SQLException e){
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public boolean updateOrderTotal(int orderId) {
-        return false;
+        String sql = "UPDATE order SET total = ? Where orderId = ?";
+        Database database = new Database();
+        try {
+            Connection con = database.getConnection();
+            con.setAutoCommit(false);
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            Scanner sc = new Scanner(System.in);
+            double total = sc.nextDouble();
+            preparedStatement.setDouble(1,total);
+            preparedStatement.setInt(2,orderId);
+            int rowsAffected = preparedStatement.executeUpdate();
+            con.commit();
+            con.setAutoCommit(true);
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
-}
