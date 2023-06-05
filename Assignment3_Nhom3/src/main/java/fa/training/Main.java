8cc78cf6762cc1370126a00252737d489768c04b
huytran2510 <135006250+huytran2510@users.noreply.github.com>
2023-06-05 22:13:57 +0700

    Update Main.java

--- Assignment3_Nhom3/src/main/java/fa/training/Main.java
+++ Assignment3_Nhom3/src/main/java/fa/training/Main.java
@@ -8,11 +8,38 @@ import fa.training.entities.Customer;
 import fa.training.entities.LineItem;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         CustomerDAO customerDAO = new CustomerDAOImpl();
-        customerDAO.getAllCustomers();
-//        customerDAO.addCustomer(new Customer(6,"Jonh swith"));
+        OrderDAO orderDAO = new OrderDAOImpl();
         LineItemDAO lineItemDAO = new LineItemDAOImpl();
-        lineItemDAO.addLineItem(new LineItem(3,2,1,2500));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choose option");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1 : customerDAO.getAllCustomers();
+            break;
+            case 2:
+//                int id = sc.nextInt();
+                orderDAO.getAllOrdersByCustomerId(1);
+            break;
+            case 3: lineItemDAO.getAllItemsByOrderId(0);
+            break;
+            case 4 : lineItemDAO.computeOrderTotal(0);
+            break;
+            case 5 :
+                customerDAO.updateCustomer(new Customer(1,"Khang"));
+                break;
+            case 6: orderDAO.updateOrderTotal(1); // chưa chạy được
+            break;
+            case 7: lineItemDAO.addLineItem(new LineItem(3,5,34,87));
+            break;
+            case 8: orderDAO.addOrder(new Order(3, Date.valueOf("2023-02-10"),3,5,54));
+            break;
+            case 9: customerDAO.addCustomer(new Customer(4,"Hùng"));
+            break;
+            case 10 : customerDAO.deleteCustomer(4);
+            break;
+        }
+
     }
 }
